cd4e0664c2ba7f76a38d1ba9f1b1a9f736843f4a
bambikun <bambikouki1433@gmail.com>
2025-07-27 23:20:14 +0900

    Update StorageTier.kt

--- src/main/kotlin/io/github/lapis256/ae2_mega_things/storage/StorageTier.kt
+++ src/main/kotlin/io/github/lapis256/ae2_mega_things/storage/StorageTier.kt
@@ -5,34 +5,48 @@ import gripe._90.megacells.item.MEGAItems
 import net.minecraft.world.level.ItemLike
 
 
-sealed class StorageTier(val kilobytes: Int, val affix: String, val component: ItemLike, val idleDrain: Double) {
+sealed class StorageTier(
+    val bytes: Long,
+    val affix: String,
+    val component: ItemLike,
+    val idleDrain: Double
+) {
     companion object {
         fun calcKilobytes(index: Int) = when (index) {
-            in 1..5 -> 1 shl (2 * (index - 1))
+            in 1..5 -> 1L shl (2 * (index - 1)) // Use Long type
             else -> throw IllegalArgumentException("Index out of range")
         }
     }
 
     open class Standard(index: Int, component: ItemLike) : StorageTier(
-        calcKilobytes(index),
+        calcKilobytes(index) * 1024L,
         "${calcKilobytes(index)}k",
         component,
         0.5 * index
     )
 
     open class Mega(index: Int, component: ItemLike) : StorageTier(
-        calcKilobytes(index) * 1000,
+        calcKilobytes(index) * 1_000_000L,
         "${calcKilobytes(index)}m",
         component,
         2.5 + 0.5 * index
     )
 
-    object SIZE1K : Standard(1, AEItems.CELL_COMPONENT_1K)
-    object SIZE4K : Standard(2, AEItems.CELL_COMPONENT_4K)
-    object SIZE16K : Standard(3, AEItems.CELL_COMPONENT_16K)
+    open class Custom(bytes: Long, affix: String, component: ItemLike, idleDrain: Double) : StorageTier(
+        bytes,
+        affix,
+        component,
+        idleDrain
+    )
+
+    // 標準 tiers
+    object SIZE1K : Custom(1L, "1", AEItems.CELL_COMPONENT_1K)
+    object SIZE4K : Custom(2147483647L, "2b" AEItems.CELL_COMPONENT_4K)
+    object SIZE16K : Custom(9223372036854775807L, "max", AEItems.CELL_COMPONENT_16K)
     object SIZE64K : Standard(4, AEItems.CELL_COMPONENT_64K)
     object SIZE256K : Standard(5, AEItems.CELL_COMPONENT_256K)
 
+    // Mega tiers
     object SIZE1M : Mega(1, MEGAItems.CELL_COMPONENT_1M)
     object SIZE4M : Mega(2, MEGAItems.CELL_COMPONENT_4M)
     object SIZE16M : Mega(3, MEGAItems.CELL_COMPONENT_16M)
